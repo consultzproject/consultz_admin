f5c0fb0fa3d2b4cc894395dc781ed6b318d66238
Cynthia <consultzproject@gmail.com>
2024-01-27 16:21:05 +0530

    env changes

--- src/constants/api-config.js
+++ src/constants/api-config.js
@@ -8,7 +8,7 @@
  export const API_CONFIG = {
   baseURL: `${import.meta.env.VITE_API_ENCRYPTION}://${
     import.meta.env.VITE_API_IP
-  }:${import.meta.env.VITE_API_PORT}`,
+  }`,
  
   // PORT2: "http://188.166.228.50:3002",
 };
